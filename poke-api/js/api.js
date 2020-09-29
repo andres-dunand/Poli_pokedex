70cf03f9ea0c1e1c1801bd62c9b5dd0266105934
andres-dunand <dunand84@hotmail.com>
2020-09-29 12:09:14 -0500

    pokemon4
    Lista de pokemon con caracteristicas

--- poke-api/js/api.js
+++ poke-api/js/api.js
@@ -1,33 +1,50 @@
 
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex);
+
 
 const fetchPokedex = () =>{
 
-    for (let cont = 1; cont <=10 ;cont ++) 
+    const promise =[];
+
+    for (let cont = 1; cont <=10 ;cont ++)
     {
-        
        const url = `https://pokeapi.co/api/v2/pokemon/${cont}`;
+       promise.push(fetch(url).then((res) => res.json()));
+    }
 
-       fetch(url)
-            .then(res =>{
-                return res.json();
-            })
-             .then(data =>{ console.log(data);
+    Promise.all(promise).then((results)=>{
 
-               const datapoke ={
-
+            const datapoke = results.map((data) => ({          
                     Nombre: data.name,
                     Orden: data.order,
                     Peso: data.weight,
                     Imagen: data.sprites['front_shiny'],
                     type: data.types.map((type)=> type.type.name).join(',')
-               };
+            }));
 
-                 console.log(datapoke);            
+        displayPokedex(datapoke);     
 
-            })
-    }
+            });
+    };
 
-}
+    const displayPokedex = (datapoke) => {
+        console.log(datapoke); 
+        const pokemonHTMLString = datapoke
+            .map( 
+                (pokesource) => `
+
+
+            <img src="${pokesource.Imagen}"/>
+            <h2>${pokesource.Orden}. ${pokesource.Nombre} </h2>
+            <p>Type: ${pokesource.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
 
 fetchPokedex();
 
